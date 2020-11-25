64080e01c5a6e9e28e5e94ab3903afcc1f6c021a
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2020-11-25 15:35:54 -0500

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:chat_app/widgets/btn_azul.dart';
+import 'package:flutter/material.dart';
+
+import 'package:chat_app/widgets/custom_input.dart';
+import 'package:chat_app/widgets/labels.dart';
+import 'package:chat_app/widgets/logo.dart';
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+
+                Logo(titulo: 'Messenger'),
+
+                _Form(),
+                
+                Labels(
+                  ruta: 'register',
+                  titulo: '¿No tienes cuenta?',
+                  subTitulo: 'Crea una ahora!'
+                ),
+
+                Text('Términos y condiciones de uso', style: TextStyle(fontWeight: FontWeight.w200)),
+              
+              ],
+            ),
+          ),
+        ),
+      ),
+   );
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  _FormState createState() => _FormState();
+}
+
+class _FormState extends State<_Form> {
+
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric( horizontal: 50 ),
+      child: Column(
+        children: <Widget>[
+          CustomInput(
+            icon: Icons.email_outlined,
+            placeholder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contraseña',
+            textController: passCtrl,
+            isPassword: true,
+          ),
+
+          BtnAzul(
+            text: 'Ingrese',
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
