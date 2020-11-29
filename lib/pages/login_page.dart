1f482a8025e5a8ff861dc4f19ad1fc55f0135bf6
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2020-11-29 12:02:10 -0500

    Chat Login and Register Terminado

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,12 @@
+import 'package:chat_app/helpers/mostrar_alerta.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:chat_app/widgets/btn_azul.dart';
 import 'package:flutter/material.dart';
 
 import 'package:chat_app/widgets/custom_input.dart';
 import 'package:chat_app/widgets/labels.dart';
 import 'package:chat_app/widgets/logo.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
 
@@ -51,6 +54,9 @@ class _FormState extends State<_Form> {
 
   @override
   Widget build(BuildContext context) {
+
+    final authService = Provider.of<AuthService>( context );        
+
     return Container(
       margin: EdgeInsets.only(top: 20),
       padding: EdgeInsets.symmetric( horizontal: 50 ),
@@ -71,7 +77,19 @@ class _FormState extends State<_Form> {
 
           BtnAzul(
             text: 'Ingrese',
-            onPressed: () {},
+            onPressed: authService.autenticando ? null : 
+            () async{
+
+              FocusScope.of(context).unfocus();
+              final loginOk = await authService.login(emailCtrl.text.trim(), passCtrl.text.trim());
+
+              if(loginOk){
+                Navigator.pushReplacementNamed(context, 'usuarios');
+
+              }else{
+                mostrarAlerta(context, "Login incorrecto", "Revise sus credenciales nuevamente");
+              }
+            },
           )
         ],
       ),
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,10 @@
-import 'package:chat_app/widgets/btn_azul.dart';
+import 'package:chat_app/helpers/mostrar_alerta.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
+import 'package:chat_app/services/auth_service.dart';
+
+import 'package:chat_app/widgets/btn_azul.dart';
 import 'package:chat_app/widgets/custom_input.dart';
 import 'package:chat_app/widgets/labels.dart';
 import 'package:chat_app/widgets/logo.dart';
@@ -52,6 +56,9 @@ class _FormState extends State<_Form> {
 
   @override
   Widget build(BuildContext context) {
+
+    final authService = Provider.of<AuthService>(context);
+
     return Container(
       margin: EdgeInsets.only(top: 20),
       padding: EdgeInsets.symmetric( horizontal: 50 ),
@@ -77,8 +84,18 @@ class _FormState extends State<_Form> {
           ),
 
           BtnAzul(
-            text: 'Ingrese',
-            onPressed: () {},
+            text: 'Crear Cuenta',
+            onPressed: authService.autenticando ? null :  
+           () async {
+              final registroOk =  await authService.register(nameCtrl.text.trim(), emailCtrl.text.trim(), passCtrl.text.trim());
+
+              if(registroOk == true){
+                Navigator.pushReplacementNamed(context, 'usuarios');
+              }else{
+                mostrarAlerta(context, 'Registro Incorrecto', registroOk);
+              }
+            
+            },
           )
         ],
       ),
